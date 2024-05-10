04c0ca29e14c90a8bfde24c37ecf5f48aecaf1b3
한민정 <zoangrak@gmail.com>
2024-05-10 20:58:24 +0900

    init

---
+++ src/main/java/com/greenfong/markit/MarkitApplication.java
@@ -0,0 +1,11 @@
+package com.greenfong.markit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkitApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MarkitApplication.class, args);
+	}
+}
---
+++ src/test/java/com/greenfong/markit/MarkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfong.markit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
